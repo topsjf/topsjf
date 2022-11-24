c3f4b02d17a28b4a4f38c693dacdea4c4f8004eb
jinfang <topjfk@163.com>
2022-11-24 10:15:42 +0000

    test: 更新脚本

--- commit.sh
+++ commit.sh
@@ -2,20 +2,33 @@
 
 set -e
 
+tag=origin
+branch=main
+
 echo -------
 echo commit git
 echo
 
+echo -e "\r\n拉取新代码..."
 git pull
+
+echo -e "\r\n查看本地状态..."
 
 git status
 
+echo -e "\r\n添加更改到本地仓库缓存..."
+
 git add -A
+
+echo -e "\r\n设置提交消息:"
 
 pnpm run commit
 
-git push -u origin main
+echo -e "\r\n提交到 ${branch} 远程仓库"
 
+git push -u $tag $branch
+
+echo
 echo -------
 echo ok!
 
